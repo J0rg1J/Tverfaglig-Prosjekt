0aa37fbaf9d862b9e681f366831bc640f172cb7c
J0rg1J <jorg1hellenesj@gmail.com>
2026-01-22 13:45:16 +0100

    laget script.js og koblet til supabase til index.html.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Supabase-tilkobling
+const supabaseUrl = "https://lllydmhdgetqhihhezsq.supabase.co";
+const supabaseKey = "sb_publishable_GQQrAyPTReEIUAbehyJRBA_6ld7H8-0";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase er koblet:", supabase);
